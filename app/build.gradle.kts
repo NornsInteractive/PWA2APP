62062036893cf56a0c505cef08ce0a6518f77d06
Casper <ksda9001@126.com>
2026-05-03 00:30:49 +0800

    单应用分支

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 android {
-    namespace = "com.norns.pwa2app"
+    namespace = "com.norns.nornsai"
     compileSdk {
         version = release(36) {
             minorApiLevel = 1
@@ -12,7 +12,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.norns.pwa2app"
+        applicationId = "com.norns.nornsai"
         minSdk = 26
         targetSdk = 36
         versionCode = 1
